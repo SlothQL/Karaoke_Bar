f6a5ec5560ffa9b2248ae2f6bb7faf8acf1c0ffd
Alina <lindenalina.al@gmail.com>
2020-06-26 14:05:16 +0100

    guest has name and age test passed

--- specs/guest_spec.rb
+++ specs/guest_spec.rb
@@ -6,5 +6,17 @@ require_relative('../guest')
 
 class TestGuest < Minitest::Test
 
+    def setup()
+        @guest1 = Guest.new("Ben", 35)
+        @guest2 = Guest.new("Alina", 32)
+    end
+
+    def test_guest_has_name()
+        assert_equal("Ben", @guest1.name)
+    end
+
+    def test_guest_has_age()
+        assert_equal(32, @guest2.age)
+    end
 
 end
